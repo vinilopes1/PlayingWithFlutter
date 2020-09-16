293d357901b7a6b041ae66cfe6b944557af2d7c2
Jose Vinicius <lopesvinicius1707@gmail.com>
2020-09-16 11:18:42 -0300

    Add logic for grid display and research logic.

--- gif_seeker/lib/main.dart
+++ gif_seeker/lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:gif_seeker/ui/home_page.dart';
 
-void main(){
+void main() {
   runApp(MaterialApp(
     home: HomePage(),
+    theme: ThemeData(
+        hintColor: Colors.white,
+        inputDecorationTheme: InputDecorationTheme(
+            enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.white)))),
   ));
 }
--- gif_seeker/lib/ui/home_page.dart
+++ gif_seeker/lib/ui/home_page.dart
@@ -9,12 +9,11 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   String _search;
 
   int _offset = 0;
 
-  Future<Map>_getGifs() async {
+  Future<Map> _getGifs() async {
     http.Response response;
 
     if (_search == null) {
@@ -22,7 +21,7 @@ class _HomePageState extends State<HomePage> {
           "https://api.giphy.com/v1/gifs/trending?api_key=RPohhNPZAbawfgqfIGqQUejnlEza7K95&limit=20&rating=g");
     } else
       response = await http.get(
-          "https://api.giphy.com/v1/gifs/search?api_key=RPohhNPZAbawfgqfIGqQUejnlEza7K95&q=$_search&limit=25&offset=$_offset&rating=g&lang=en");
+          "https://api.giphy.com/v1/gifs/search?api_key=RPohhNPZAbawfgqfIGqQUejnlEza7K95&q=$_search&limit=19&offset=$_offset&rating=g&lang=en");
     return json.decode(response.body);
   }
 
@@ -30,13 +29,111 @@ class _HomePageState extends State<HomePage> {
   void initState() {
     super.initState();
 
-    _getGifs().then((map){
+    _getGifs().then((map) {
       print(map);
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        title: Image.network(
+            "https://developers.giphy.com/static/img/dev-logo-lg.7404c00322a8.gif"),
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: TextField(
+              decoration: InputDecoration(
+                  labelText: "Search",
+                  labelStyle: TextStyle(color: Colors.white),
+                  border: OutlineInputBorder()),
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              textAlign: TextAlign.center,
+              onSubmitted: (text) {
+                setState(() {
+                  _search = text;
+                  _offset = 0;
+                });
+              },
+            ),
+          ),
+          Expanded(
+              child: FutureBuilder(
+                  future: _getGifs(),
+                  // ignore: missing_return
+                  builder: (context, snapshot) {
+                    switch (snapshot.connectionState) {
+                      case ConnectionState.waiting:
+                      case ConnectionState.none:
+                        return Container(
+                          width: 200.0,
+                          height: 200.0,
+                          alignment: Alignment.center,
+                          child: CircularProgressIndicator(
+                              valueColor:
+                                  AlwaysStoppedAnimation<Color>(Colors.white),
+                              strokeWidth: 5.0),
+                        );
+                      default:
+                        if (snapshot.hasError)
+                          return Container(color: Colors.red);
+                        else
+                          return _createGifTable(context, snapshot);
+                    }
+                  }))
+        ],
+      ),
+    );
+  }
+
+  int _getCount(List data) {
+    if (_search == null) {
+      return data.length;
+    } else {
+      return data.length + 1;
+    }
+  }
+
+  Widget _createGifTable(BuildContext context, AsyncSnapshot snapshot) {
+    return GridView.builder(
+        padding: EdgeInsets.all(10.0),
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2, crossAxisSpacing: 10.0, mainAxisSpacing: 10.0),
+        itemCount: _getCount(snapshot.data["data"]),
+        itemBuilder: (context, index) {
+          if (_search == null || index < snapshot.data["data"].length) {
+            return GestureDetector(
+              child: Image.network(
+                snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+                height: 300.0,
+                fit: BoxFit.cover,
+              ),
+            );
+          } else {
+            return Container(
+              child: GestureDetector(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Icon(Icons.add, color: Colors.white, size: 70.0),
+                    Text("More Gifs...",
+                        style: TextStyle(color: Colors.white, fontSize: 22.0))
+                  ],
+                ),
+                onTap: (){
+                  setState(() {
+                    _offset += 19;
+                  });
+                },
+              ),
+            );
+          }
+        });
   }
 }
